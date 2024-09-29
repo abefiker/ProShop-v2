cde55102b01f5434f388bd5f3620670a2be586c5
abfiker <new94abemelek@gmail.com>
2024-09-29 11:50:02 +0300

    user and order model prepared properly

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,10 +1,34 @@
 const mongoose = require("mongoose");
-
+import { isEmail } from 'validator';
 const userSchema = new mongoose.Schema({
-  name: String,
-  email:String,
-  password: String,
-  isAdmin: Boolean
+  name: {
+    type:String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    match: /^[a-zA-Z0-9]+$/,
+  },
+  email:{
+    type:String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [ isEmail, 'invalid email' ]
+  },
+  password:{
+    type: String,
+    required: true,
+    minlength: 6,
+    select: false, // Never return password in response
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  }
+},{
+  timestamps: true, // Automatically adds createdAt and updatedAt fields.
+ // select: false, // Never return password in response, can be useful for security.
 });
 
 const User = mongoose.model('User', userSchema);
