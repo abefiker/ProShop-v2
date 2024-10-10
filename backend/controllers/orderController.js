9405e948337eb3b5cf9507066828bf669f9514e6
abfiker <new94abemelek@gmail.com>
2024-10-10 15:42:44 +0300

    createOrder setted up in orderSlice

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -48,10 +48,13 @@ exports.getMyOrders = asyncHandler(async (req, res) => {
 //@route GET /api/orders/:id
 //@access Private
 exports.getOrderById = asyncHandler(async (req, res) => {
-  const order = await Order.findById(req.params.id).populate('user','name email');
-  if(order){
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+  if (order) {
     res.status(200).json(order);
-  }else{
+  } else {
     res.status(404);
     throw new Error('Resource not found');
   }
