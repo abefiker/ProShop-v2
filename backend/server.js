dc7b4feeb28cb3c13433d5bca1f03d557f718efd
abfiker <new94abemelek@gmail.com>
2024-11-14 16:49:19 +0300

    final happend

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,9 @@ app.get('/api/config/paypal', (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
   });
 } else {
   app.get('/', (req, res) => {
